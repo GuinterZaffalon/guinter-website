c4c7501c941003345112392da2fd31174e692e8b
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-10-02 20:26:37 -0300

    feat: deploy latest version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <h1>olhaaa</h1>
+     <main className="h-screen w-screen bg-slate-200 items-center justify-center">
+        <p className="text-black">Em construção!</p>
+     </main>
   );
 }
