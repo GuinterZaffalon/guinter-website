516b38828a99228f6c6e9f879de98f4aa2b515c8
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-10-07 18:25:42 -0300

    feat: versão final do site na v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,29 @@ import icon from "../imagens/pipiipipi 1.png"
 import Image from 'next/image'
 
 export default function Home() {
+  const links = [
+    {
+      color: "bg-card1",
+      text: "Meus projetos pessoais! 💻",
+      link: "https://github.com/GuinterZaffalon",
+    },
+    {
+      color: "bg-card2",
+      text: "Meu perfil no LinkedIn 🤝",
+      link: "https://www.linkedin.com/in/guinter-zaffalon-857131241/",
+    },
+    {
+      color: "bg-card3",
+      text: "E-Mail para contato! 📧",
+      link: "https://github.com",
+    },
+  ];
+
   return (
     <div className="w-full h-screen bg-background_page">
       <div className="max-w-2xl mx-auto h-screen">
         <div className="h-48 w-48 mx-auto">
-          <div className="h-48 w-48">
+          <div className="h-48 w-48 p-6 ">
             <Image
               src={icon}
               alt="icon do us"
@@ -18,10 +36,20 @@ export default function Home() {
         </div>
         <div className="text-center p-6">
           <h1 className="text-4xl font-bold">Guinter Zaffalon</h1>
-          <p className="text- mt-3">Mobile Developer</p>
+          <p className="text-xl mt-3 font-normal">Mobile Developer</p>
         </div>
-        <div className="">
-
+        <div className="flex flex-col gap-10">
+          {links.map(({ text, color, link }, index) => {
+            return (
+              <a href={link} key={index} target="_blank">
+                <div
+                  className={`w-80 sm:w-96 mx-auto ${color} text-center text-xl font-bold py-3 border-2 border-black shadow-custom hover:shadow-none transition-all hover:translate-x-1 hover:translate-y-1`}
+                >
+                  {text}
+                </div>
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
