3f92b73290f76ce8a967300339f88d88278add57
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-10-07 18:15:50 -0300

    feat: cores adicionadas do novo esquema de cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import InstagramIcon from "@/componentes/intagramIcon";
 
-export default function Home() {
-
-  var gambiarraForte: String = "</>"
-
-  return (
-    <main className="min-h-screen bg-gradient-to-r from-backgroundgradient1 to-backgroundgradient2 p-4">
-      <div className="grid-rows-8 h-full w-full justify-center items-center">
-        <div className="row-span-1 max-h-full flex justify-end p-3">
-          <p className="text-white text-xl">{gambiarraForte}</p>
-        </div>
+export default function PaginaTeste() {
+    return(
+        <div className="w-full h-screen bg-background_page">
+            <div className="max-w-2xl mx-auto h-screen">
+                <Image 
+                src={icon}
+                alt="icon do us"
+                />
+            </div>
         <div className="row-span-1 h-full w-full justify-center flex flex-col text-center gap-1">
           <p className="text-5xl">Guinter Zaffalon</p>
           <p className="text-2xl">Mobile Developer</p>
