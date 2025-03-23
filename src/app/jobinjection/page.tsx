863409b942f29852167d5286ab95eca9f07ae308
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-03-23 20:10:33 -0300

    style e form

--- src/app/jobinjection/page.tsx
+++ src/app/jobinjection/page.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function JobInjection() {
+    const [job, setJob] = useState({ position: '', seniority: '', notWords: '' });
+    const { register, handleSubmit, setValue, control } = useForm();
+
+
+
     return (
         <div className="bg-[#1e293b] w-full h-screen">
-            <div className="flex items-center justify-center pt-16">
+            <div className="flex items-center justify-center pt-16 gap-3">
                 <h1 className="text-4xl text-white font-mono">Job Injection</h1>
+                <form
+                    action=""
+                    // onChange={handleSubmit(onSubmiData)}
+                    className="flex flex-col gap-2">
+
+                </form>
             </div>
         </div>
     );
