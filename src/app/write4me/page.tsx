21dc33ea2b5c1c807eadd7d94c3d33776329d2b7
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-04-07 15:59:25 -0300

    função com o template e fetch funcionando

--- src/app/write4me/page.tsx
+++ src/app/write4me/page.tsx
@@ -62,7 +62,7 @@ export default function Write4Me() {
     };
 
     return (
-        <div className="bg-white w-full h-screen">
+        <div className="bg-white w-full min-h-screen">
             <div className="flex items-center justify-center pt-10 pb-5 gap-3 flex-col">
                 <h1 className="text-4xl text-white font-lewis bg-black p-2 font-bold dark:text-white">WRITE 4 ME</h1>
                 <form action=""
