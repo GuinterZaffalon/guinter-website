6978e9ea1ba47423cecad996f5b60e648280af41
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-22 20:43:07 -0300

    instalação do shadcn!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,78 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        backgroundgradient1: "#0F2845",
-        backgroundgradient2: "#717297",
-        background_page: "#44AF69",
-        card1: "#F8333C",
-        card2: "#FCAB10",
-        card3: "#2B9EB3",
-        boxshadow: "#DBD5B5",
-        "space-blue": '#0a1a3c',
-      },
-      boxShadow: {
-				custoum: "10px 10px 0px -1px rgba(0,0,0,1)",
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				backgroundgradient1: '#0F2845',
+				backgroundgradient2: '#717297',
+				background_page: '#44AF69',
+				card1: '#F8333C',
+				card2: '#FCAB10',
+				card3: '#2B9EB3',
+				boxshadow: '#DBD5B5',
+				'space-blue': '#0a1a3c',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
 			},
-      fontFamily: {
-        virgil: ["Virgil", "serif"],
-        jetbrainsmono: ["JetBrains Mono", "monospace"],
-        lewis: ["Lewis", "serif"],
-        mono: ['"VT323"', "monospace"],
-      },
-      keyframes: {
-        'slide-left': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        },
-      },
-      animation: {
-        'slide-left': 'slide-left 8s linear infinite',
-      }
-    },
-  },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-  ]
+			screens: {
+				smallHeight: { raw: '(max-height: 550px)' },
+				w800: { max: '800px' },
+				w700: { max: '700px' },
+				w600: { max: '600px' },
+				w500: { max: '500px' },
+				w450: { max: '450px' },
+				w400: { max: '400px' },
+			},
+		}
+	},
+	plugins: [
+		require("@tailwindcss/aspect-ratio"),
+		require("tailwindcss-animate")
+	]
 };
 export default config;
