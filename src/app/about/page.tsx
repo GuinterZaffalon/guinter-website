03553199597c3a43494994c6c4075a94e7ed21cd
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-23 14:42:18 -0300

    experiencia correta!

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,14 +7,14 @@ import Skills from "../../components/sections/skills";
 export default function About() {
   return (
     <div className="font-base">
-      <h1 className="mb-8 text-2xl font-heading sm:text-4xl">About</h1>
+      <h1 className="mb-8 text-2xl font-heading sm:text-4xl">Sobre</h1>
 
       <div className="mb-10 text-base sm:text-lg">
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
-          consequatur, harum pariatur provident rerum placeat magni voluptas
-          consectetur in exercitationem nobis aut, molestiae iure possimus
-          aspernatur nesciunt laudantium ab atque.
+          Apaixonado por desenvolver soluções e obcecado por tudo
+          que amo! Empenhado em dar meu melhor em tudo que prático, é assim
+          que me tornei faixa preta e campeão Sul Americano no Taekwondo, e da mesma
+          forma estou evoluindo minha senioriedade em desenvolvimento.
         </p>
       </div>
 
--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,30 +5,35 @@ const PAST_ROLES: {
   startDate: string
   endDate: string
 }[] = [
-  {
-    company: 'ACME Solutions',
-    role: 'CEO',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'March 2023',
-    endDate: 'Now',
-  },
-  {
-    company: 'Google',
-    role: 'Software Engineer',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'March 2023',
-  },
-  {
-    company: 'Vercel',
-    role: 'Developer Advocate',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'June 2018',
-  },
-]
+    {
+      company: 'Freelancer',
+      role: 'Desenvolvedor Fullstack e Mobile',
+      description:
+        `Criação de produtos próprios com Flutter, aplicando arquitetura moderna, provendo cerca de 20% de
+        economia de tempo no desenvolvimento geral. Com aplicação em processo de aprovação na Google Play.
+        Desenvolvimento de aplicações Web Fullstack utilizando PHP e Next.js, criando soluções escaláveis e
+        modernizando interfaces, melhorando o fluxo e o desempenho do usuário final.
+        Implementação de bancos de dado, tanto dockerizados, tanto Supabase ou Firebase.
+        Desenvolvimento de interfaces responsivas e acessíveis, seguindo boas práticas de UX/UI com
+        ferramentas como Figma`,
+      startDate: 'Janeiro 2025',
+      endDate: 'Atual',
+    },
+    {
+      company: 'Creare Sistemas',
+      role: 'Desenvolvimedor',
+      description:
+        `Desenvolvimento de aplicativos Flutter, aplicando visão computacional para controle de frotas em grande
+empresas, aumentando em 80% a produtividade do pátio de veículos. Também fazendo uso de
+WebSocket para configurar equipamentos, permitindo o técnico operar sem rede.
+Criação de aplicações Web Full Stack com Next.js, utilizando BFF (Backend for Frontend) e integração
+com APIs RESTful e GraphQL.
+Desenvolvimento de sistemas ao longo de todo o ciclo de vida, desde a concepção e levantamento de
+requisitos até a entrega e manutenção.
+Implementação de banco de dados PostgreSQL em ambientes dockerizados.`,
+      startDate: 'Junho 2024',
+      endDate: 'Dezembro 2024',
+    },
+  ]
 
 export default PAST_ROLES
