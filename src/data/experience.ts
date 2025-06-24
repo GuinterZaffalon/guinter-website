5cef2229f24ab7037e96e1ee2d536ec7a463d799
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-06-24 09:26:55 -0300

    feat: correção de textos

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,35 +5,34 @@ const PAST_ROLES: {
   startDate: string
   endDate: string
 }[] = [
-    {
-      company: 'Freelancer',
-      role: 'Desenvolvedor Fullstack e Mobile',
-      description:
-        `Criação de produtos próprios com Flutter, aplicando arquitetura moderna, provendo cerca de 20% de
-        economia de tempo no desenvolvimento geral. Com aplicação em processo de aprovação na Google Play.
-        Desenvolvimento de aplicações Web Fullstack utilizando PHP e Next.js, criando soluções escaláveis e
-        modernizando interfaces, melhorando o fluxo e o desempenho do usuário final.
-        Implementação de bancos de dado, tanto dockerizados, tanto Supabase ou Firebase.
-        Desenvolvimento de interfaces responsivas e acessíveis, seguindo boas práticas de UX/UI com
-        ferramentas como Figma`,
-      startDate: 'Janeiro 2025',
-      endDate: 'Atual',
-    },
-    {
-      company: 'Creare Sistemas',
-      role: 'Desenvolvimedor',
-      description:
-        `Desenvolvimento de aplicativos Flutter, aplicando visão computacional para controle de frotas em grande
-empresas, aumentando em 80% a produtividade do pátio de veículos. Também fazendo uso de
-WebSocket para configurar equipamentos, permitindo o técnico operar sem rede.
-Criação de aplicações Web Full Stack com Next.js, utilizando BFF (Backend for Frontend) e integração
-com APIs RESTful e GraphQL.
-Desenvolvimento de sistemas ao longo de todo o ciclo de vida, desde a concepção e levantamento de
-requisitos até a entrega e manutenção.
+  {
+    company: 'Freelancer',
+    role: 'Desenvolvedor Fullstack e Mobile',
+    description: `
+Criação de produtos próprios com Flutter, aplicando arquitetura moderna e proporcionando cerca de 20% de economia de tempo no desenvolvimento geral. Uma das aplicações está em processo de aprovação na Google Play.
+
+Desenvolvimento de aplicações Web Fullstack utilizando PHP e Next.js, criando soluções escaláveis e modernizando interfaces, o que resultou em melhorias no fluxo e no desempenho para o usuário final.
+
+Implementação de bancos de dados, tanto dockerizados quanto com Supabase ou Firebase.
+
+Desenvolvimento de interfaces responsivas e acessíveis, seguindo boas práticas de UX/UI com ferramentas como o Figma.`,
+    startDate: 'Janeiro 2025',
+    endDate: 'Atual',
+  },
+  {
+    company: 'Creare Sistemas',
+    role: 'Desenvolvedor',
+    description: `
+Desenvolvimento de aplicativos Flutter, aplicando visão computacional para controle de frotas em grandes empresas, aumentando em 80% a produtividade do pátio de veículos. Também utilizei WebSocket para configurar equipamentos, permitindo que técnicos operassem mesmo sem conexão de rede.
+
+Criação de aplicações Web Fullstack com Next.js, utilizando a abordagem BFF (Backend for Frontend) e integrando APIs RESTful e GraphQL.
+
+Atuação em todas as etapas do ciclo de desenvolvimento de sistemas, desde a concepção e levantamento de requisitos até a entrega e manutenção.
+
 Implementação de banco de dados PostgreSQL em ambientes dockerizados.`,
-      startDate: 'Junho 2024',
-      endDate: 'Dezembro 2024',
-    },
-  ]
+    startDate: 'Junho 2024',
+    endDate: 'Dezembro 2024',
+  },
+]
 
 export default PAST_ROLES
